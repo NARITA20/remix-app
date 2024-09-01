058ea9213b41d78b6af1bc0024f7dc205879bc12
NARITA20 <anikentaro8@yahoo.ne.jp>
2024-09-01 11:08:20 +0900

    add marker

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,5 @@
 import type { MetaFunction } from "@remix-run/cloudflare";
-import koike from '.vscode/images/umi.jpg'
+import koike from '.vscode/images/umi.jpg';
 
 export const meta: MetaFunction = () => {
   return [
@@ -16,33 +16,40 @@ export default function Index() {
     <div className="font-sans p-4">
       <img src={koike} className="App-logo" alt="logo" />
 
-      <p style={{textAlign: "center"}}> 
-          私を一言で表すと、
-        </p>
-      <h1 style={{textAlign: "center"}}>真面目</h1>
-      <p style={{textAlign: "center"}}> 
-          な人間です。
-        </p>
+      <p style={{ textAlign: "center" }}>
+        私を一言で表すと、
+      </p>
+      
+      <h1 
+        style={{ 
+          textAlign: "center", 
+          backgroundImage: "linear-gradient(transparent 60%, #ffcc99 60%)",
+          display: "inline-block",
+        }}
+      >
+        真面目
+      </h1>
+      
+      <p style={{ textAlign: "center" }}>
+        な人間です。
+      </p>
+      
       <ul className="list-disc mt-4 pl-6 space-y-2">
-      {/* .marker-border-2{
-      background:linear-gradient(transparent 60%, #ffcc99 0%);} */}
-      <h2 className="marker-border-2" style={{textAlign: "center"}}>
-        Skills
-      </h2>
-        
+        <h2 className="marker-border-2" style={{ textAlign: "center" }}>
+          Skills
+        </h2>
 
-        <li style={{textAlign: "center"}}>
+        <li style={{ textAlign: "center" }}>
           <a
             className="text-blue-700 underline visited:text-purple-900"
             target="_blank"
             href="https://remix-app-7jy.pages.dev/book"
             rel="noreferrer"
-            
           >
             book
           </a>
         </li>
-        <li style={{textAlign: "center"}}>
+        <li style={{ textAlign: "center" }}>
           <a
             className="text-blue-700 underline visited:text-purple-900"
             target="_blank"
