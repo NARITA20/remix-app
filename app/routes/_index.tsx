424c17fd503a30eaed910001267ebc925f8e6603
NARITA20 <anikentaro8@yahoo.ne.jp>
2024-09-08 13:27:20 +0900

    add text and add github link

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -43,21 +43,46 @@ export default function Index() {
         </div>
         </ul>
         <li style={{ textAlign: "center" }} className="text-4xl">
-          java
+          Java
         </li>
         <p></p>
         <p style={{ textAlign: "center" }}>
             2年間学んでいます。授業でオセロAIを作成しました。
-          </p>
-        {/* <li style={{ textAlign: "center" }}>
-          <a
+        </p>
+        <li style={{ textAlign: "center" }} className="text-4xl">
+          Python
+        </li>
+        <p></p>
+        <p style={{ textAlign: "center" }}>
+            3年間学んでいます。自然言語処理の勉強や、turtleも使いました。
+            最近では、授業の一環で高校生にPythonを教えたりもしています。
+        </p>
+        <li style={{ textAlign: "center" }} className="text-4xl">
+          C++
+        </li>
+        <p></p>
+        <p style={{ textAlign: "center" }}>
+            半年間学んでします。ポインターなど初めて扱い、理解する事に苦労しました。
+        </p>
+        <li style={{ textAlign: "center" }} className="text-4xl">
+          開発経験
+        </li>
+        <p></p>
+        <p style={{ textAlign: "center" }}>
+            今回のwebサイト制作で初めて開発を行いました。
+            cloudfrareでサーバーをレンタルし、githubとcloudfrareを連携しました。
+            以下に私のgithubのリンクを記載します。
+        </p>
+        <a
             className="text-blue-700 underline visited:text-purple-900"
             target="_blank"
-            href="https://remix-app-7jy.pages.dev/book"
+            href="https://github.com/NARITA20?tab=repositories"
             rel="noreferrer"
           >
-            book
+            https://github.com/NARITA20?tab=repositories
           </a>
+        {/* <li style={{ textAlign: "center" }}>
+          
         </li>
         <li style={{ textAlign: "center" }}>
           <a
