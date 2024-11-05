36dec0b958132a8dbf63f10e990d4c34e6170e5b
NARITA20 <anikentaro8@yahoo.ne.jp>
2024-11-05 16:32:21 +0900

    small image

--- app/components/Images.tsx
+++ app/components/Images.tsx
@@ -8,7 +8,7 @@ function ShapeExample() {
     <Container>
       <Row>
         
-        <Col xs={{span:4 ,offset:2}} md={{span:3, offset:2}}>
+        <Col xs={{span:3 ,offset:2}} md={{span:3, offset:2}}>
           <Image src='img/Aboutme2.jpg' roundedCircle />
         </Col>
       </Row>
