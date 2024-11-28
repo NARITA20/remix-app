48bcc2d570b1a2f18652c904b5a16dfaa6016e68
NARITA20 <anikentaro8@yahoo.ne.jp>
2024-11-28 21:49:48 +0900

    change the componet

--- app/components/Images.tsx
+++ app/components/Images.tsx
@@ -9,7 +9,7 @@ function ShapeExample() {
       <Row>
         
         {/* <Col xs={{span:2 ,offset:5}} md={{span:6, offset:2}}> */}
-        <Col xs={{span:4,offset:5}} md={4}>
+        <Col xs={{span:4,offset:4}} md={4}>
           <Image src='img/Aboutme2.jpg' roundedCircle />
           </Col>
         {/* </Col> */}
