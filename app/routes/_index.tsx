83bc00ddf599ff12f2ca48773078f189f877993a
NARITA20 <anikentaro8@yahoo.ne.jp>
2024-11-28 19:24:16 +0900

    change link

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -61,11 +61,10 @@ export default function Index() {
 <div style={{display: "flex",justifyContent: "center", alignItems: "center", marginTop: "10px" }}>
 
 
-<div className="container">
-  <link rel="stylesheet" href="style.css">
+<div className="container" >
+  <link rel="stylesheet" href="style.css"></link>
   <div className="item"><Images /> </div>
   <div className="item">成田健太郎 </div>
-  </link>
 </div>
 
 </div>
