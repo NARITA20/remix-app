acc398eddb5f4eee59f8ac52ec6fd80c950c1591
NARITA20 <anikentaro8@yahoo.ne.jp>
2024-11-28 22:21:22 +0900

    change the componet

--- app/components/Images.tsx
+++ app/components/Images.tsx
@@ -7,13 +7,14 @@ function ShapeExample() {
   return (
     <Container>
       <Row>
-        
-        <Col xs={3}md={3} style={{ marginLeft: '20px' }}>
+      <Col xs={3}md={3} >
+        </Col>
+        <Col xs={3}md={3} >
           <Image src='img/Aboutme2.jpg' roundedCircle />
         </Col>
         <Col xs={3} md={3}>
         <div className="text-3xl">
-          成田健太郎
+          成田 健太郎
           </div>
         </Col>
       </Row>
